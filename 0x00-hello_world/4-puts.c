7cace013f0ecb6384cc1842e2e02c81d1ad3f554
root <ogeeletaiwo433@gmail.com>
2022-09-08 15:00:12 -0700

    puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main(void)
 /**
  * main is the entry point
- */	
+ */
+	int main(void)
 {
-	puts("/"programming is like building a multilingual puzzle!\n");
-	return (0);
+		puts("\"programming is like building a multilingual puzzle!\n");
+		return (0);
 }	
